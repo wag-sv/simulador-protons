42aba9c846eeadbc84b34d524bf014c7e8843714
Wagner S. Vieira <w.sv@icloud.com>
2025-07-01 11:03:21 -0400

    feat: implementa Google Tag Manager para remarketing e conversões
    - Adiciona GTM script na head com ID GTM-KLTF2ZLX
    - Implementa fallback noscript no body para usuários sem JavaScript
    - Utiliza componente Script do Next.js com strategy afterInteractive para otimização
    - Habilita rastreamento de conversões e campanhas de remarketing
    - Código implementado conforme especificações do proprietário da Protons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +25,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KLTF2ZLX');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLTF2ZLX"
+            height="0" 
+            width="0" 
+            style={{display:"none",visibility:"hidden"}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
